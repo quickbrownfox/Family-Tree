5f0439c3f87cb2ad7a1d3a7c0b5da6a7478917b7
drypa <drypa@drypa-desktop.(none)>
2010-12-26 17:43:25 +0300

    scaffold added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Familytree::Application.routes.draw do
+  resources :women
+
+  resources :men
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
---
+++ test/unit/helpers/women_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class WomenHelperTest < ActionView::TestCase
+end
