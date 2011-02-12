b66b7f39920807b0f2a465031e6960edea98119c
drypa <drypa@drypa-desktop.(none)>
2011-02-12 19:44:58 +0300

    delete unused files

--- app/models/man.rb
+++ app/models/man.rb
@@ -1,6 +1,5 @@
 class Man < ActiveRecord::Base
-  belongs_to :Man, :foreign_key => :father_id
-  belongs_to :Woman, :foreign_key => :mother_id
+#  belongs_to :Man, :foreign_key => :father_id
   validates :first_name, :presence => true
   validates :last_name, :presence => true
 end
